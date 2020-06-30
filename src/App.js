9228d0b1d6080b564edbf25901919988d7f41077
Laura Kenny <l.j.kenny@hotmail.com>
2020-06-30 21:39:07 +0100

    adds actions and reducers.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './App.css';
+import * as styles from './styles'
 
-function App() {
-  return (
-    <div className="App">
-      React
-    </div>
-  );
+class App extends Component {
+
+  render() {
+  
+    return (
+      <div className="App">
+        <div style={styles.styles}>
+          Styles
+        </div>
+        <button>Button 1</button>
+        React
+      </div>
+    );
+  }
 }
+ 
+const AppInstance = new App()
+
+console.log(AppInstance)
 
 export default App;
